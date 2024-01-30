a4902b95dafac2955309e5b5fd9e6d98ec3a472b
Aasif Raza <aasif.raza@dedalus.eu>
2024-01-30 23:07:11 +0530

    afdding eureka clients

--- employee-service/src/main/java/com/raza/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/raza/employeeservice/EmployeeServiceApplication.java
@@ -2,12 +2,14 @@ package com.raza.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.raza.employeeservice.feignclients")
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
