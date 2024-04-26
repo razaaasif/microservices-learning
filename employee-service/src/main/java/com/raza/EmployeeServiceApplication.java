315a0637c7b9122096806cf0a7ddb54bb8f4cf89
Aasif Raza <aasif.raza@dedalus.eu>
2024-04-26 22:04:38 +0530

    added all service to eureka client

--- employee-service/src/main/java/com/raza/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/raza/EmployeeServiceApplication.java
@@ -2,22 +2,17 @@ package com.raza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
-
-//	@Bean
-//	public RestTemplate restTemplte() {
-//		return new RestTemplate();
-//	}
-
-	@Bean
-	public WebClient webClient() {
+    @Bean
+    WebClient webClient() {
 		return WebClient.builder().build();
 	}
 
