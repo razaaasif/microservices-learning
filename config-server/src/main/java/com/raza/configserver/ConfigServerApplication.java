d1c277ac3dc6726402bd4ae3622ea87e55ab99bb
razaaasif <aasifraza9123@gmail.com>
2024-05-06 12:38:36 +0530

    learned config server

--- config-server/src/main/java/com/raza/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/raza/configserver/ConfigServerApplication.java
@@ -1,16 +1,17 @@
-package com.raza.apigateway;
+package com.raza.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
-public class ApiGatewayApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
