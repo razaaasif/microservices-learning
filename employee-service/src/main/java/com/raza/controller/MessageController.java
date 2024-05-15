2832057b3e3dcebf8b05be5cefdcb1be13bb3f48
razaaasif <aasifraza9123@gmail.com>
2024-05-15 10:58:06 +0530

    spring cloud bus learning

--- employee-service/src/main/java/com/raza/controller/MessageController.java
+++ employee-service/src/main/java/com/raza/controller/MessageController.java
@@ -1,4 +1,4 @@
-package com.raza.department.controller;
+package com.raza.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
-@RequestMapping("/api/departments")
+@RequestMapping("/api/employees")
 public class MessageController {
     @Value("${spring.boot.message}")
     private String message;
 
-    @GetMapping("message")
+    @GetMapping("/users/message")
     public String getMessage(){
         return this.message;
     }
